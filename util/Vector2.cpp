e639c1ef80906a7d2b91d7d9352b6f66c73f6fa7
Michael <84879204+M-Trevizo@users.noreply.github.com>
2025-01-21 03:04:39 -0800

    Bug fixes

--- util/Vector2.cpp
+++ util/Vector2.cpp
@@ -1,11 +1,11 @@
 #include "Vector2.h";
 
-Vector2::Vector2() {
+util::Vector2::Vector2() {
 	x = 0;
 	y = 0;
 }
 
-Vector2::Vector2(x, y) {
+util::Vector2::Vector2(double x, double y) {
 	this->x = x;
 	this->y = y;
 }
--- util/Vector2.h
+++ util/Vector2.h
@@ -1,9 +1,15 @@
-class Vector2 {
+#pragma once
+
+namespace util {
+	class Vector2;
+}
+
+class util::Vector2 {
 
 public:
 	double x;
 	double y;
 
 	Vector2();
-	Vector2(double x, double y)
+	Vector2(double x, double y);
 };
